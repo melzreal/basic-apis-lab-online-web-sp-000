9e12060e1c4408132eb9c3f025de92c5edd782be
Melissa Real <melissa.real@hotmail.com>
2019-06-21 16:42:08 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,7 @@
 class RepositoriesController < ApplicationController
 
   def search
-     @resp = Faraday.get'https://api.github.com/search/repositories?q=tetris?&client_id=3321637f8b0641b4a2f2&client_secret=fb5bb0472511aa0ce0f6a2b85b6cc754a68e824f'
+     @resp = Faraday.get'https://api.github.com/search/repositories?q=params[:query]?&client_id=3321637f8b0641b4a2f2&client_secret=fb5bb0472511aa0ce0f6a2b85b6cc754a68e824f'
     # do |req|
     #       req.params['q'] = params[:query]
     #       # req.params['client_id'] = ENV['GIT_CLIENT_ID']
